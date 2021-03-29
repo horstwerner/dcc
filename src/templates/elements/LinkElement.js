a22737aee87a1f8505e172a5cc9a5481ead38401
Mabuse <horst.toffaneto.werner@gmail.com>
2021-03-28 19:30:47 -0700

    Link element has url instead of urlAttribute, handlebars allowed.

--- src/templates/elements/LinkElement.js
+++ src/templates/elements/LinkElement.js
@@ -1,22 +1,21 @@
 import P from 'prop-types';
 import TemplateElement, {StylePropType} from "@/templates/elements/TemplateElement";
-import {resolveAttribute} from "@/graph/Cache";
 import {Link} from "@/components/Generators";
+import {fillIn} from "@symb/util";
 
 export default class LinkElement extends TemplateElement {
 
   static key = 'link';
 
   static propTypes = {...TemplateElement.propTypes,
-    urlAttribute: P.string.isRequired,
+    url: P.string.isRequired,
     text: P.string,
     image: P.string,
     style: P.shape(StylePropType)
   }
 
   static create ({descriptor, data}) {
-    const { urlAttribute } = descriptor;
-    const url = resolveAttribute(data, urlAttribute);
-    return url && Link({...descriptor, url});
+    const { url } = descriptor;
+    return url && Link({...descriptor, url: fillIn(url, data)});
   }
 }
