8c25e3373a1b2eb6e569be5daddfbf4e4cba155b
Mabuse <horst.toffaneto.werner@gmail.com>
2021-03-28 20:17:33 -0700

    detail fixes

--- src/components/CardSet.js
+++ src/components/CardSet.js
@@ -31,46 +31,6 @@ class CardSet extends Component {
     super(props, parent, div);
   }
 
-  // createChildrenForLod(lod) {
-  //   const {nodes, arrangement, template, onClick, clickMode} = this.innerProps;
-  //
-  //   this.lod = lod;
-  //
-  //   switch (lod) {
-  //     case
-  //     'rect':
-  //       if (this.childByKey) {
-  //         Object.keys(this.childByKey).forEach(key => this.childByKey[key].destroy());
-  //       }
-  //       this.childByKey = {};
-  //       const size = arrangement.getSize();
-  //       const offset = arrangement.getOffset();
-  //       this.canvas = document.createElement('canvas');
-  //       this.canvas.setAttribute('width', size.width);
-  //       this.canvas.setAttribute('height', size.height);
-  //       setStyle(this.canvas, {position: 'absolute', left: offset.x, top: offset.y, pointerEvents: 'none'});
-  //       const context = this.canvas.getContext('2d');
-  //       this.dom.appendChild(this.canvas);
-  //       const {width, height} = template.getSize(ARRANGEMENT_DEFAULT);
-  //       arrangement.forEachRasterpos(nodes, (node, rasterPos) => {
-  //         context.fillStyle = template.getCardColor(node);
-  //         context.fillRect(rasterPos.x - offset.x, rasterPos.y - offset.y, rasterPos.scale * width, rasterPos.scale * height);
-  //       });
-  //       break;
-  //     default:
-  //       const childDescriptors = [];
-  //       if (this.canvas) {
-  //         this.canvas.remove();
-  //         this.canvas = null;
-  //       }
-  //       arrangement.forEachRasterpos(nodes, (node, rasterPos) => {
-  //         childDescriptors.push(
-  //             Card_({key: node.getUniqueKey(), spatial: rasterPos, data: node, parentSet: this, onClick, template, clickMode})._Card
-  //         );
-  //       });
-  //   }
-  // };
-
   createChildDescriptors(props) {
 
     const { arrangement, nodes, template, onClick, clickMode, options } = props;
@@ -83,7 +43,6 @@ class CardSet extends Component {
     });
     return childDescriptors;
   }
-
 }
 
 ComponentFactory.registerType(CardSet);
