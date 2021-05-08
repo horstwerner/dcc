31e9795dff8717d26773937a1d1b61337ef9f4cc
hwerner <werner@brainframe.de>
2021-05-08 13:54:54 -0700

    Fixed staggered graph layout problems

--- src/components/GraphViz.js
+++ src/components/GraphViz.js
@@ -149,12 +149,9 @@ class GraphViz extends Component {
     vizNodes.forEach(vizNode => lanes[vizNode.depth].push(vizNode));
     lanes = lanes.filter(lane => lane && lane.length > 0);
 
-    let maxNodesPerLane = Math.max(...lanes.map(lane => lane.length));
-    if (maxNodesPerLane > LANE_BREAK_THRESHOLD) {
-      maxNodesPerLane = Math.ceil(maxNodesPerLane / 2);
-    }
+    let maxNodesPerLane = Math.max(...lanes.map(lane => (lane.length > LANE_BREAK_THRESHOLD ? Math.ceil(lane.length / 2) : lane.length)));
 
-    const maxChildH = 0.85 * h / ((maxNodesPerLane || 1) + 1);
+    const maxChildH = 0.8 * h / (maxNodesPerLane + 1);
     const maxChildW = 0.5 * w / (lanes.length || 1);
     const maxAR = maxChildW / (maxChildH || 1);
     let childW, childH;
@@ -171,33 +168,54 @@ class GraphViz extends Component {
     const rasterH = netLaneH / maxNodesPerLane;
 
     const xStep = (w - 2 * childW) / ((lanes.length - 1) || 1);
-    let xCursor = childW;
 
-    lanes.forEach(lane => {
-          const staggered = lane.length > LANE_BREAK_THRESHOLD;
-          const lanePositions = staggered ? Math.ceil(lane.length / 2) -1 : lane.length - 1;
-          let yCursor = (netLaneH - rasterH * lanePositions + childH) / 2;
-          const yStep = rasterH; //netLaneH / ((lane.length - 1) || 1);
-          lane.forEach(node => {
-            node.rank = sum(node.inEdges.map(edge => get(vizNodesByKey[edge.sourceKey],'pos.y'))) / (node.inEdges.length || 1);
-          });
-          lane.sort((a, b) => a.rank - b.rank);
-          let count = 0;
-          lane.forEach(node => {
-            const offset = count++ % 2;
-            if (!staggered) {
-              node.pos = {x: xCursor, y: yCursor};
-            } else  {
-              node.pos = {x: xCursor + (offset - 0.5) * 1.1 * childW, y: yCursor + 0.5 * offset * yStep}
-            }
-            yCursor += yStep * (staggered ? offset : 1);
-          });
-          xCursor += xStep;
+    for (let laneIdx = 0; laneIdx < lanes.length; laneIdx++) {
+      const lane = lanes[laneIdx];
+      const staggered = lane.length > LANE_BREAK_THRESHOLD;
+
+      const xCursor = childW + laneIdx * xStep +
+          (laneIdx === 0 && staggered ? 0.25 * childW : 0) -
+          (laneIdx === lanes.length - 1 && staggered ? 0.25 * childW : 0);
+
+      const staggeredXCursor = [
+        xCursor - 0.5 * 1.1 * childW,
+        xCursor + 0.5 * 1.1 * childW,
+      ];
+
+      const laneTerminals = [];
+      let laneChildW = 0;
+      const lanePositions = staggered ? Math.ceil(lane.length / 2) + (lane.length % 2 === 0 ? 0.5 : 0)  : lane.length;
+      let yCursor = Math.max(0, (netLaneH - rasterH * (lanePositions - 1)) / 2) + 0.5 * childH;
+      const yStep = rasterH; //netLaneH / ((lane.length - 1) || 1);
+      lane.forEach(node => {
+        node.rank = sum(node.inEdges.map(edge => get(vizNodesByKey[edge.sourceKey],'pos.y'))) / (node.inEdges.length || 1);
+      });
+      lane.sort((a, b) => a.rank - b.rank);
+      let count = 0;
+
+      lane.forEach(node => {
+        node.laneIdx = laneIdx;
+        const offset = count++ % 2;
+        if (!staggered) {
+          node.pos = {x: xCursor, y: yCursor};
+        } else  {
+          node.pos = {x: staggeredXCursor[offset], y: yCursor + 0.5 * offset * yStep}
         }
-    );
+        yCursor += yStep * (staggered ? offset : 1);
+
+        node.terminals = laneTerminals;
+        node.template = TemplateRegistry.getTemplateForSingleCard(node.graphNode.getTypeUri(), viewName || 'default');
+        const { width, height } = node.template.getSize();
+        node.spatial = fit(childW, childH, width, height, node.pos.x - 0.5 * childW, node.pos.y - 0.5 * childH);
+        laneChildW = Math.max(laneChildW, node.spatial.scale * width);
+      });
+
+      laneTerminals[0] = (staggered ? staggeredXCursor[0]: xCursor) - 0.6 * laneChildW;
+      laneTerminals[1] = (staggered ? staggeredXCursor[1]: xCursor) + 0.6 * laneChildW;
+
+    }
 
     const lines = [];
-    const edgeDist = 0.6 * childW;
     // ####################### C R E A T E   E D G E S #################################################
     vizNodes.forEach(vizNode => {
       if (!vizNode.outEdges) return;
@@ -205,33 +223,32 @@ class GraphViz extends Component {
         const targetNode = vizNodesByKey[targetKey];
         if (targetNode.pos.x > vizNode.pos.x) { // forward edge
           lines.push([vizNode.pos,
-            {x: vizNode.pos.x + edgeDist, y: vizNode.pos.y},
-            {x: targetNode.pos.x - edgeDist, y: targetNode.pos.y},
+            {x: vizNode.terminals[1], y: vizNode.pos.y},
+            {x: targetNode.terminals[0], y: targetNode.pos.y},
             targetNode.pos
           ]);
         } else { // backward edge
           const dySrc = (Math.sign(targetNode.pos.y - vizNode.pos.y) || -4) * 0.28 * childH
           const dyTrg = Math.abs(targetNode.pos.y - vizNode.pos.y) < 0.6 * childH ? dySrc : -dySrc;
           lines.push([vizNode.pos,
-            {x: vizNode.pos.x + edgeDist, y: vizNode.pos.y},
-            {x: vizNode.pos.x + edgeDist, y: vizNode.pos.y + dySrc},
-            {x: targetNode.pos.x - edgeDist, y: targetNode.pos.y + dyTrg},
-            {x: targetNode.pos.x - edgeDist, y: targetNode.pos.y},
+            {x: vizNode.terminals[1], y: vizNode.pos.y},
+            {x: vizNode.terminals[1], y: vizNode.pos.y + dySrc},
+            {x: targetNode.terminals[0], y: targetNode.pos.y + dyTrg},
+            {x: targetNode.terminals[0] , y: targetNode.pos.y},
             targetNode.pos
           ]);
         }
       });
     });
 
-    const roundDist = edgeDist - 0.5 * childW;
+    const roundDist = 0.07 * childW;
     const children = [];
     children.push(Svg_({style:{pointerEvents: 'none'}, width: w, height: h, children: lines.map(line => createSvgPath(line, roundDist))})._Svg);
 
     vizNodes.forEach(vizNode => {
-      const template = TemplateRegistry.getTemplateForSingleCard(vizNode.graphNode.getTypeUri(), viewName || 'default');
-      const { width, height } = template.getSize();
-      const cardNode = createPreprocessedCardNode(vizNode.graphNode, createContext(), template, null);
-      children.push(Card_({data: cardNode, template, onClick: onNodeClick, clickMode: CLICK_OPAQUE, spatial: fit(childW, childH, width, height, vizNode.pos.x - 0.5 * childW, vizNode.pos.y - 0.5 * childH)})._Card)
+      const cardNode = createPreprocessedCardNode(vizNode.graphNode, createContext(), vizNode.template, null);
+      const {spatial, template} = vizNode;
+      children.push(Card_({data: cardNode, template, onClick: onNodeClick, clickMode: CLICK_OPAQUE, spatial})._Card)
     });
 
     return children;
